8314bd2d0e71f661020be89fc16eb6de5d41d1fb
bakhtiyarTayir <bakhtiyarTayir@gmail.com>
2023-06-06 18:32:31 +0500

    admin lte

---
+++ resources/views/admin/post/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layouts.main')
+@section('content')
+    <div class="row">
+        <div class="col">
+            <p>posts</p>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['namespace'=> 'Main'], function(){
+    Route::get('/', [App\Http\Controllers\Main\IndexController::class, '__invoke']);
+
+});
+
+Route::group(['namespace'=> 'Admin', 'prefix' => 'admin'], function() {
+    Route::group(['namespace' => 'Main'], function(){
+        Route::get('/', [App\Http\Controllers\Admin\Main\IndexController::class, '__invoke']);
+    });
+});
