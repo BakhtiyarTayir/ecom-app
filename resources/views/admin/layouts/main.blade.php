3135e5eddddb94654c41e54d8ebb8ac43245c1ab
bakhtiyarTayir <bakhtiyarTayir@gmail.com>
2023-06-09 19:22:04 +0500

    favicon

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard</title>
+    
+    <title>Blog</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="{{ asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback') }}">
@@ -13,6 +14,7 @@
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
 
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/images/loader.gif') }}" type="image/x-icon">
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
